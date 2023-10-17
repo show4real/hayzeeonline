a3c904ec2e1fc0b29ae0980cf2bda8118f78be2d
Showole Oladayo <show4ril@gmail.com>
2023-10-17 18:49:06 +0100

    changes

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/storage/*', '/images/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/storage/*', '/images/*', '/detailedproducts/*'],
 
     'allowed_methods' => ['*'],
 
