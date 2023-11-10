422a8a04376da24d80497919d3da93b378b47b68
Showole Oladayo <show4ril@gmail.com>
2023-11-10 04:10:59 +0100

    email template update

--- resources/views/mail/verify.blade.php
+++ resources/views/mail/verify.blade.php
@@ -1,147 +1,74 @@
- 
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Hayzeeonline Referrer Sign up</title>
+    <title>Email Confirmation</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-    /* Styles from the previous template... */
-    body,
-    p,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-        margin: 0;
-        padding: 0;
-    }
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+        }
 
-    body {
-        font-family: Arial, sans-serif;
-        line-height: 1.6;
-    }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #e0e0e0;
+            border-radius: 6px;
+        }
 
-    /* Container styles */
-    .container {
-        max-width: 600px;
-        margin: 0 auto;
-        padding: 20px;
-    }
+        .logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
 
-    /* Header styles */
-    .header {
-        background-color: #f0f0f0;
-        padding: 10px;
-        text-align: center;
-    }
+        .logo img {
+            max-width: 150px;
+            height: auto;
+        }
 
-    .header h1 {
-        color: #333333;
-    }
+        .header {
+            background-color: #f0f0f0;
+            padding: 10px;
+            text-align: center;
+        }
 
-    /* Product styles */
-    .product {
-        border: 1px solid #e0e0e0;
-        margin-bottom: 20px;
-    }
+        .header h1 {
+            color: #333333;
+        }
 
-    .product-img {
-        text-align: center;
-        padding: 10px;
-    }
+        .content {
+            margin-top: 20px;
+            color: #333333;
+        }
 
-    .product-img img {
-        max-width: 100%;
-        height: auto;
-    }
+        .btn {
+            display: inline-block;
+            padding: 16px 36px;
+            font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+            font-size: 16px;
+            color: #ffffff;
+            background-color: #4CAF50;
+            text-decoration: none;
+            border-radius: 6px;
+            margin-top: 20px;
+        }
 
-    .product-info {
-        padding: 10px;
-    }
+        .footer {
+            text-align: center;
+            padding: 10px;
+            background-color: #f0f0f0;
+            margin-top: 20px;
+        }
 
-    .product-name {
-        font-size: 18px;
-        font-weight: bold;
-        color: #333333;
-        margin-bottom: 10px;
-    }
-
-    .product-price {
-        font-size: 16px;
-        color: #333333;
-    }
-
-    /* Total styles */
-    .total {
-        text-align: right;
-        margin-top: 20px;
-    }
-
-    .total span {
-        font-size: 18px;
-        font-weight: bold;
-        color: #333333;
-    }
-
-    /* Footer styles */
-    .footer {
-        text-align: center;
-        padding: 10px;
-        background-color: #f0f0f0;
-    }
-
-    /* Company logo */
-    .logo {
-        text-align: center;
-        margin-bottom: 20px;
-    }
-
-    .logo img {
-        max-width: 150px;
-        height: auto;
-    }
-
-    /* Order details */
-    .order-details {
-        margin-bottom: 20px;
-        text-align: right;
-    }
-
-    .order-details span {
-        font-size: 14px;
-        color: #666666;
-    }
-
-    /* Customer information */
-    .customer-info {
-        margin-bottom: 20px;
-    }
-
-    .customer-info h2 {
-        font-size: 18px;
-        font-weight: bold;
-        color: #333333;
-        margin-bottom: 10px;
-    }
-
-    .customer-info p {
-        font-size: 14px;
-        color: #666666;
-    }
-
-    /* Description or message */
-    .description {
-        margin-bottom: 20px;
-    }
-
-    .description p {
-        font-size: 16px;
-        color: #333333;
-    }
+        .footer p {
+            color: #99AAB5;
+            font-family: Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;
+            font-size: 12px;
+            line-height: 24px;
+        }
     </style>
 </head>
 
@@ -149,52 +76,26 @@
     <div class="container">
         <!-- Company logos -->
         <div class="logo">
-            <img src="{{ asset('logo/logo5.png') }}" alt="Hayzeeonline">
-
+            <img src="{{ asset('logo/logo5.png') }}" alt="Company Logo">
         </div>
 
         <!-- Header -->
         <div class="header">
-            <h1>Welcome on Board joining our Affiliate market!</h1>
+            <h1>Welcome aboard to our Affiliate Market! </h1>
         </div>
 
-
-        <!-- Customer information -->
-        <div class="customer-info">
-            Refer us today and get your percentage instantly
-            Generate your referral code as soon as you verify your Email
-        </div>
-
-       
-
-        <div class="product">
-            
-            <div class="product-info">
-              
-
-                <a href="https://hayzeeonline.com/verify/{{$email}}" target="_blank" style="text-decoration:none; color:black; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">
-                Verify Email</a>
-
-               
-            </div>
-        </div>
-
-
-
-
-        <!-- Description or message -->
-        <div class="description">
-            <p>Thank you for Joining us. </p>
+        <!-- Content -->
+        <div class="content">
+            <p>Hello {{$referrer->name}},</p>
+            <p>Thank you for signing up. Click the button below to confirm your email address and get started.</p>
+            <a href="https://hayzeeonline.com/verify/{{$email}}" class="btn">Confirm Email Address</a>
         </div>
 
         <!-- Footer -->
         <div class="footer">
-            <p>08037586863</p>
-            <div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:0px;" align="center"><div style="cursor:auto;color:#99AAB5;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:12px;line-height:24px;text-align:center;">
-      Sent by Hayzee Computer Resources • <a href="https://hayzeeonline.com/" style="color:#1EB0F4;text-decoration:none;" target="_blank">Visit our Webiste</a> • <a href="https://twitter.com/hayzeeonline" style="color:#1EB0F4;text-decoration:none;" target="_blank">@hayzeeonline</a>
-    </div></td></tr><tr><td style="word-break:break-word;font-size:0px;padding:0px;" align="center"><div style="cursor:auto;color:#99AAB5;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:12px;line-height:24px;text-align:center;">
-     Spectral Business Oyo State, Ibadan, Ibadan
-    </div></td></tr></tbody></table></div
+            <p>Contact us at support@hayzeeonline.com</p>
+            <p>Sent by Hayzee Computer Resources • <a href="https://hayzeeonline.com" target="_blank">Visit our Website</a> • <a href="https://twitter.com/example" target="_blank">@example</a></p>
+            <p>Spectral Business center, The Polytechnic Ibadan, Oyo State. NG</p>
         </div>
     </div>
 </body>
