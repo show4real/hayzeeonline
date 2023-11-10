c6ef7b31e6a1de728aafd8213aa64128a7b029db
Showole Oladayo <show4ril@gmail.com>
2023-11-10 03:59:37 +0100

    email template update

--- resources/views/mail/verify.blade.php
+++ resources/views/mail/verify.blade.php
@@ -1,164 +1,202 @@
+ 
+ <!DOCTYPE html>
+<html>
+
 <head>
-  <title></title>
-  <!--[if !mso]><!-- -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!--<![endif]-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<style type="text/css">
-  #outlook a { padding: 0; }
-  .ReadMsgBody { width: 100%; }
-  .ExternalClass { width: 100%; }
-  .ExternalClass * { line-height:100%; }
-  body { margin: 0; padding: 0; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
-  table, td { border-collapse:collapse; mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
-  img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; }
-  p { display: block; margin: 13px 0; }
-</style>
-<!--[if !mso]><!-->
-<style type="text/css">
-  @media only screen and (max-width:480px) {
-    @-ms-viewport { width:320px; }
-    @viewport { width:320px; }
-  }
-</style>
-<!--<![endif]-->
-<!--[if mso]>
-<xml>
-  <o:OfficeDocumentSettings>
-    <o:AllowPNG/>
-    <o:PixelsPerInch>96</o:PixelsPerInch>
-  </o:OfficeDocumentSettings>
-</xml>
-<![endif]-->
-<!--[if lte mso 11]>
-<style type="text/css">
-  .outlook-group-fix {
-    width:100% !important;
-  }
-</style>
-<![endif]-->
+    <title>Hayzeeonline Referrer Sign up</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    /* Styles from the previous template... */
+    body,
+    p,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        margin: 0;
+        padding: 0;
+    }
 
-<!--[if !mso]><!-->
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
-    <style type="text/css">
+    body {
+        font-family: Arial, sans-serif;
+        line-height: 1.6;
+    }
 
-        @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+    /* Container styles */
+    .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+    }
 
+    /* Header styles */
+    .header {
+        background-color: #f0f0f0;
+        padding: 10px;
+        text-align: center;
+    }
+
+    .header h1 {
+        color: #333333;
+    }
+
+    /* Product styles */
+    .product {
+        border: 1px solid #e0e0e0;
+        margin-bottom: 20px;
+    }
+
+    .product-img {
+        text-align: center;
+        padding: 10px;
+    }
+
+    .product-img img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    .product-info {
+        padding: 10px;
+    }
+
+    .product-name {
+        font-size: 18px;
+        font-weight: bold;
+        color: #333333;
+        margin-bottom: 10px;
+    }
+
+    .product-price {
+        font-size: 16px;
+        color: #333333;
+    }
+
+    /* Total styles */
+    .total {
+        text-align: right;
+        margin-top: 20px;
+    }
+
+    .total span {
+        font-size: 18px;
+        font-weight: bold;
+        color: #333333;
+    }
+
+    /* Footer styles */
+    .footer {
+        text-align: center;
+        padding: 10px;
+        background-color: #f0f0f0;
+    }
+
+    /* Company logo */
+    .logo {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    .logo img {
+        max-width: 150px;
+        height: auto;
+    }
+
+    /* Order details */
+    .order-details {
+        margin-bottom: 20px;
+        text-align: right;
+    }
+
+    .order-details span {
+        font-size: 14px;
+        color: #666666;
+    }
+
+    /* Customer information */
+    .customer-info {
+        margin-bottom: 20px;
+    }
+
+    .customer-info h2 {
+        font-size: 18px;
+        font-weight: bold;
+        color: #333333;
+        margin-bottom: 10px;
+    }
+
+    .customer-info p {
+        font-size: 14px;
+        color: #666666;
+    }
+
+    /* Description or message */
+    .description {
+        margin-bottom: 20px;
+    }
+
+    .description p {
+        font-size: 16px;
+        color: #333333;
+    }
     </style>
-  <!--<![endif]--><style type="text/css">
-  @media only screen and (min-width:480px) {
-    .mj-column-per-100, * [aria-labelledby="mj-column-per-100"] { width:100%!important; }
-  }
-</style>
 </head>
-<body style="background: #F9F9F9;">
-  <div style="background-color:#F9F9F9;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]-->
-  <style type="text/css">
-    html, body, * {
-      -webkit-text-size-adjust: none;
-      text-size-adjust: none;
-    }
-    a {
-      color:#1EB0F4;
-      text-decoration:none;
-    }
-    a:hover {
-      text-decoration:underline;
-    }
-  </style>
-<div style="margin:0px auto;max-width:640px;background:transparent;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:transparent;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 0px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:0px;" align="center"><table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="center" border="0"><tbody><tr><td style="width:138px;"><a href="https://discordapp.com/" target="_blank"><img alt="" title="" height="38px" src="https://cdn.discordapp.com/email_assets/2ec94ed90b8e95d764f2a1c96f33139e.png" style="border:none;border-radius:;display:block;outline:none;text-decoration:none;width:100%;height:38px;" width="138"></a></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--><div style="max-width:640px;margin:0 auto;box-shadow:0px 1px 5px rgba(0,0,0,0.1);border-radius:4px;overflow:hidden"><div style="margin:0px auto;max-width:640px;background:#7289DA url(https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png) top center / cover no-repeat;"><!--[if mso | IE]>
-      <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:640px;">
-        <v:fill origin="0.5, 0" position="0.5,0" type="tile" src="https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png" />
-        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
-      <![endif]--><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#7289DA url(https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png) top center / cover no-repeat;" align="center" border="0" background="https://cdn.discordapp.com/email_assets/f0a4cc6d7aaa7bdf2a3c15a193c6d224.png"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:57px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:undefined;width:640px;">
-      <![endif]--><div style="cursor:auto;color:white;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:36px;font-weight:600;line-height:36px;text-align:center;">
-     Welcome on Board joining our Affiliate market!</div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table><!--[if mso | IE]>
-        </v:textbox>
-      </v:rect>
-      <![endif]--></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--><div style="margin:0px auto;max-width:640px;background:#ffffff;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#ffffff;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:40px 70px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:0px 0px 20px;" align="left"><div style="cursor:auto;color:#737F8D;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:16px;line-height:24px;text-align:left;">
-            <p><img src="{{ asset('logo/logo5.png') }}" alt="Hayzeeonline" title="None" width="500" style="height: auto;"></p>
 
-  <h2 style="font-family: Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-weight: 500;font-size: 20px;color: #4F545C;letter-spacing: 0.27px;">Hey SmilesDavis,</h2>
-<p>Wowwee! Thanks for registering an account with Hayzee Computer Resources!</p>
-<p>  Refer us today and get your percentage instantly
-            Generate your referral code as soon as you verify your Email</p>
+<body>
+    <div class="container">
+        <!-- Company logos -->
+        <div class="logo">
+            <img src="{{ asset('logo/logo5.png') }}" alt="Hayzeeonline">
 
-          </div></td></tr><tr>
-            <td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center">
-            <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0">
-            <tbody>
-                <tr>
-                    <td style="border:none;border-radius:3px;color:white;cursor:auto;padding:15px 19px;" align="center" valign="middle" bgcolor="#7289DA">
-                    <a href="https://hayzeeonline.com/verify/{{$email}}" style="text-decoration:none;line-height:100%;background:#7289DA;color:white;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:15px;font-weight:normal;text-transform:none;margin:0px;" target="_blank">
-            Verify Email
-          </a></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--></div><div style="margin:0px auto;max-width:640px;background:transparent;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:transparent;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:0px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;"><div style="font-size:1px;line-height:12px;">&nbsp;</div></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--><div style="margin:0 auto;max-width:640px;background:#ffffff;box-shadow:0px 1px 5px rgba(0,0,0,0.1);border-radius:4px;overflow:hidden;"><table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#ffffff;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;font-size:0px;padding:0px;"><!--[if mso | IE]>
-      <table border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]-->
-      <!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="640" align="center" style="width:640px;">
-        <tr>
-          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-      <![endif]--><div style="margin:0px auto;max-width:640px;background:transparent;"><table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:transparent;" align="center" border="0"><tbody><tr><td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;"><!--[if mso | IE]>
-      <table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:640px;">
-      <![endif]--><div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:0px;" align="center"><div style="cursor:auto;color:#99AAB5;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:12px;line-height:24px;text-align:center;">
+        </div>
+
+        <!-- Header -->
+        <div class="header">
+            <h1>Welcome on Board joining our Affiliate market!</h1>
+        </div>
+
+
+        <!-- Customer information -->
+        <div class="customer-info">
+            Refer us today and get your percentage instantly
+            Generate your referral code as soon as you verify your Email
+        </div>
+
+       
+
+        <div class="product">
+            
+            <div class="product-info">
+              
+
+                <a href="https://hayzeeonline.com/verify/{{$email}}" target="_blank" style="text-decoration:none; color:black; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">
+                Verify Email</a>
+
+               
+            </div>
+        </div>
+
+
+
+
+        <!-- Description or message -->
+        <div class="description">
+            <p>Thank you for Joining us. </p>
+        </div>
+
+        <!-- Footer -->
+        <div class="footer">
+            <p>08037586863</p>
+            <div aria-labelledby="mj-column-per-100" class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;"><table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0"><tbody><tr><td style="word-break:break-word;font-size:0px;padding:0px;" align="center"><div style="cursor:auto;color:#99AAB5;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:12px;line-height:24px;text-align:center;">
       Sent by Hayzee Computer Resources • <a href="https://hayzeeonline.com/" style="color:#1EB0F4;text-decoration:none;" target="_blank">Visit our Webiste</a> • <a href="https://twitter.com/hayzeeonline" style="color:#1EB0F4;text-decoration:none;" target="_blank">@hayzeeonline</a>
     </div></td></tr><tr><td style="word-break:break-word;font-size:0px;padding:0px;" align="center"><div style="cursor:auto;color:#99AAB5;font-family:Whitney, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:12px;line-height:24px;text-align:center;">
      Spectral Business Oyo State, Ibadan, Ibadan
-    </div></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></td></tr></tbody></table></div><!--[if mso | IE]>
-      </td></tr></table>
-      <![endif]--></div>
-
+    </div></td></tr></tbody></table></div
+        </div>
+    </div>
 </body>
+
+</html>
