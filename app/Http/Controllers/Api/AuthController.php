36058dfa40c8c015f48d811909a115744cb92a94
Showole Oladayo <show4ril@gmail.com>
2023-11-10 13:07:21 +0100

    changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Traits\SignUpTrait;
+use App\Models\Referrer;
+use App\Models\User;
 
 
 class AuthController extends Controller
