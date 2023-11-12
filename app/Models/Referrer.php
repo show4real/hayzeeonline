043f4acbc8ff79f5d781303e80f6aeffbf37e43c
Showole Oladayo <show4ril@gmail.com>
2023-11-12 09:03:39 +0100

    auth update

--- app/Models/Referrer.php
+++ app/Models/Referrer.php
@@ -9,8 +9,26 @@ class Referrer extends Model
 {
     use HasFactory;
 
+    protected $appends = ['email', 'phone'];
+
       public function user()
     {
         return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+     public function getEmailAttribute()
+    {
+        $user = User::where('id', $this->user_id)->first();
+        if ($user) {
+            return $user->email;
+        }
+    }
+
+    public function getPhoneAttribute()
+    {
+        $user = User::where('id', $this->user_id)->first();
+        if ($user) {
+            return $user->phone;
+        }
     }
 }
