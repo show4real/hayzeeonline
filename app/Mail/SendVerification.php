b2da06b843cd4b28d628adef13b9bfefa5fea159
Showole Oladayo <show4ril@gmail.com>
2023-11-14 01:23:53 +0100

    changes

--- app/Mail/SendVerification.php
+++ app/Mail/SendVerification.php
@@ -28,7 +28,7 @@ class SendVerification extends Mailable
     public function build()
     {
         return $this->subject('Hayzee Computer Resources Referrer Email Verification')
-         ->from('hayzee@hayzeeonline.com', 'Hayzee Computer Resources')
+         ->from('test@hayzeeonline.com', 'Hayzee Computer Resources')
         ->view('mail.verify')
                     ->with('referrer', $this->referrer);
     }
