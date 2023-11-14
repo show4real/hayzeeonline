f761464e3e8670fde92f0de9c5321f5f5e93928e
Showole Oladayo <show4ril@gmail.com>
2023-11-14 01:03:15 +0100

    changes

--- app/Jobs/VerifyMail.php
+++ app/Jobs/VerifyMail.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Mail\SendVerification;
+use Illuminate\Support\Facades\Mail;
 
 class VerifyMail implements ShouldQueue
 {
