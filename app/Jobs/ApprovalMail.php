81e3e019c4690fb600b97f34f8d3d5a2e3e3cb6a
Showole Oladayo <show4ril@gmail.com>
2023-11-15 11:49:53 +0100

    search model update

--- app/Jobs/ApprovalMail.php
+++ app/Jobs/ApprovalMail.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Mail\SendApproval;
+use Illuminate\Support\Facades\Mail;
 
 class ApprovalMail implements ShouldQueue
 {
