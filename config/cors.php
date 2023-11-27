8527ebd0dd1ab6c46baa13c8a1f19b20193f75cb
Showole Oladayo <show4ril@gmail.com>
2023-11-27 11:55:35 +0100

    changes

--- config/cors.php
+++ config/cors.php
@@ -31,7 +31,7 @@ return [
     /*
      * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
      */
-    'allowed_origins' => ['*', 'https://apiv2.hayzeeonline.com'],
+    'allowed_origins' => ['https://apiv2.hayzeeonline.com'],
 
     /*
      * Patterns that can be used with `preg_match` to match the origin.
