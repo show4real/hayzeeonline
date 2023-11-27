0b8236cdf2fecfa2d908c4b20f16a35ff77514a1
Showole Oladayo <show4ril@gmail.com>
2023-11-27 14:31:51 +0100

    changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        // \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'CheckAdmin' => \App\Http\Middleware\CheckAdmin::class,
         'CheckReferrer' => \App\Http\Middleware\CheckReferrer::class,
-        'Cors' => \App\Http\Middleware\Cors::class,
+        // 'Cors' => \App\Http\Middleware\Cors::class,
     ];
 }
