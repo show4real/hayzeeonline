37522368da268e9d3828cad6234d4c0de9787307
Showole Oladayo <show4ril@gmail.com>
2023-11-27 16:29:59 +0100

    changes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,4 @@
 <?php
-
-header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
