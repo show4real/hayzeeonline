1cb5a05ea716cf98aac7f458c3c274327105d599
Showole Oladayo <show4ril@gmail.com>
2023-12-01 02:46:30 +0100

    changes

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -39,17 +39,37 @@ class PaymentController extends Controller
 
     public function completeOrder(Request $request)
     {
-        $paymentReference = $request->payment_reference;
-        $data = array(
-        "reference" => $request->payment_reference,
-        "email" => $request->email,
-        );
+        $reference = $request->payment_reference;
+
+        $curl = curl_init();
+  
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.paystack.co/transaction/verify/:".$reference,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+            "Authorization: Bearer sk_test_288b97779ff8c9f143ab3887da7e36d2f67297dc",
+            "Cache-Control: no-cache",
+            ),
+        ));
+        
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+        
+        // if ($err) {
+        //     echo "cURL Error #:" . $err;
+        // } else {
+        //     echo $response;
+        // }
 
 
-        $paymentDetails = Paystack::getPaymentData($data);
-
-
-        if ($paymentDetails['data']['status'] === 'success') {
+        if ($response['data']['status'] === 'success') {
 
             $user = User::firstOrCreate(['email' => $request->email], [
                 'phone' => $request->phone,
@@ -82,11 +102,11 @@ class PaymentController extends Controller
                 }
             }
                 
-                return response()->json(['message' => 'Payment successful', 'data' => $paymentDetails]);
+                return response()->json(['message' => 'Payment successful', 'data' => $order]);
 
         } else {
                 
-            return response()->json(['message' => 'Payment failed', 'data' => $paymentDetails], 422);
+            return response()->json(['message' => 'Payment failed', 'data' => $err], 422);
         }
     }
 }
