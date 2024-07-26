251836e98a0f85e51b252e349a1b77ff17e458b5
Showole Oladayo <show4ril@gmail.com>
2024-07-26 05:24:03 +0100

    update

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -59,7 +59,6 @@ class ShopController extends Controller
             ->sort($request->sort)
             ->filterByPrice($request->price[0], $request->price[1], $request->search_all)
             ->orderByRaw("availability = 1 DESC")
-            ->inRandomOrder()
             ->paginate($request->rows, ['*'], 'page', $request->page);
 
              $notice = Notice::first()->notice;
